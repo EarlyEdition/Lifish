a70816ef2c5b2999a0b3cd1eb08f1e68e08b3ae4
earlyedition <earlygitedition@gmail.com>
2023-11-24 12:32:25 -0800

    Update misc.cpp

--- src/misc.cpp
+++ src/misc.cpp
@@ -32,7 +32,7 @@ namespace {
 
 /// Version number. If Version is left empty, then compile date in the format
 /// DD-MM-YY and show in engine_info.
-const string Version = "";
+const string Version = "Beta";
 
 /// Our fancy logging facility. The trick here is to replace cin.rdbuf() and
 /// cout.rdbuf() with two Tie objects that tie cin and cout to a file stream. We
@@ -75,10 +75,12 @@ const string engine_info(bool to_uci) {
   string month, day, year;
   stringstream ss, date(__DATE__); // From compiler, format is "Sep 21 2008"
 
-  ss << "Stockfish" << Version << setfill('0');
+  ss << "Lifish " << Version << setfill('0');
 
-  ss << (to_uci  ? "\nid author ": " by ")
-     << "T. Romstad, M. Costalba, J. Kiiski, G. Linscott";
+  ss << (Is64Bit ? " 64-bit" : "")
+     << (HasPext ? " BMI2" : (HasPopCnt ? " POPCNT" : ""))
+     << (to_uci  ? "\nid author ": " by ")
+     << "Tord Romstad, Marco Costalba, Joona Kiiski, Gary Linscott and Gary Hobson";
 
   return ss.str();
 }
