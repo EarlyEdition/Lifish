49f819911113bd5ee032ecdbc649333e6df8a58c
Gary Hobson <73174332+EarlyEdition@users.noreply.github.com>
2023-11-30 18:30:56 -0800

    Slight tweak to king safety
    https://github.com/mcostalba/Stockfish/pull/46
    
    Co-Authored-By: Gary Linscott <554932+glinscott@users.noreply.github.com>

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -236,7 +236,7 @@ namespace {
     ei.attackedBy[Us][ALL_PIECES] |= ei.attackedBy[Us][PAWN] = ei.pi->pawn_attacks(Us);
 
     // Init king safety tables only if we are going to use them
-    if (pos.non_pawn_material(Us) >= QueenValueMg)
+    if (pos.non_pawn_material(Us) > QueenValueMg + PawnValueMg)
     {
         ei.kingRing[Them] = b | shift_bb<Down>(b);
         b &= ei.attackedBy[Us][PAWN];
